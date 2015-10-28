aa5ec560c07de65acdb49b758bba477aa7a9b325
Rodrigo Mariano <rodmariano13@gmail.com>
2015-10-28 16:53:57 -0200

    Teste Carros OK.

--- tests/test_w2pt_ctl_dflt.py
+++ tests/test_w2pt_ctl_dflt.py
@@ -15,32 +15,60 @@ class TestCtlDefault(W2PTestCase):
 	def setUp(self):
 		W2PTestCase.setUp(self,default,vitrine)
 		construirDependencias()
-
-	def test_index(self):
 		inicializarDb(default.db)
 
-		query=default.db.carro.id>0
-		rows=default.db(query).select(orderby=default.db.carro.id) 
+	# OK
+	# def test_index(self):
+	# 	inicializarDb(default.db)
 
-		result=default.index()
+	# 	query=default.db.carro.id>0
+	# 	rows=default.db(query).select(orderby=default.db.carro.id) 
 
-		self.assertEqual(result['titulo'],'Ofertas')
-		for row in rows:
-			self.assertEqual(True,self.inside(default.URL('detalhes', args=row.id),result['vitrine']))
-			self.assertEqual(True,self.inside(row.marca,result['vitrine']))
-			self.assertEqual(True,self.inside(row.modelo,result['vitrine']))
-			self.assertEqual(True,self.inside(row.ano,result['vitrine']))
-			self.assertEqual(True,self.inside(row.estado,result['vitrine']))
-			self.assertEqual(True,self.inside(row.cor,result['vitrine']))
-			for item in row.itens:
-				self.assertEqual(True,self.inside(item,result['vitrine']))	
-			self.assertEqual(True,self.inside(row.descr,result['vitrine']))
-			self.assertEqual(True,self.inside(_web2py_brasil_utils.Moeda(row.valor),result['vitrine']))
+	# 	result=default.index()
 
+	# 	self.assertEqual(result['titulo'],'Ofertas')
+	# 	for row in rows:
+	# 		self.assertEqual(True,self.inside(default.URL('detalhes', args=row.id),result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.marca,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.modelo,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.ano,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.estado,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.cor,result['vitrine']))
+	# 		for item in row.itens:
+	# 			self.assertEqual(True,self.inside(item,result['vitrine']))	
+	# 		self.assertEqual(True,self.inside(row.descr,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(_web2py_brasil_utils.Moeda(row.valor),result['vitrine']))
+
+	# OK
 	# def test_carros(self):
-	# 	self.assertEqual('Text','Text')
-	# def test_detalhes(self):
-	# 	self.assertEqual('55','55')
+	# 	default.request.args.append('Novos')
+	# 	estado = default.request.args(0)
+
+	# 	query=default.db.carro.estado==estado[:-1].capitalize()  
+	# 	rows=default.db(query).select(orderby=default.db.carro.id) 
+
+	# 	titulo='Carros %s' % estado.capitalize()
+
+	# 	result=default.carros()
+
+	# 	self.assertEqual(result['titulo'],titulo)
+
+	# 	for row in rows:
+	# 		self.assertEqual(True,self.inside(default.URL('detalhes', args=row.id),result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.marca,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.modelo,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.ano,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.estado,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(row.cor,result['vitrine']))
+	# 		for item in row.itens:
+	# 			self.assertEqual(True,self.inside(item,result['vitrine']))	
+	# 		self.assertEqual(True,self.inside(row.descr,result['vitrine']))
+	# 		self.assertEqual(True,self.inside(_web2py_brasil_utils.Moeda(row.valor),result['vitrine']))
+
+	def test_detalhes(self):
+		self.assertEqual('55','55')
+
+	
 	# def test_pesquisa(self):
 	# 	self.assertEqual('Text','Text')
 	# def test_admin(self):
